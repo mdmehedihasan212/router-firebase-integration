94fb1aa16de47127ea836dce277175e9eedd4ca6
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-09 02:55:50 +0600

    create form login and register component

--- src/Components/LogIn/LogIn.js
+++ src/Components/LogIn/LogIn.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './LogIn.css';
 
 const LogIn = () => {
     return (
         <div>
-            <h1>LogIn</h1>
+            <h1>Please Login!!</h1>
+            <form className='login-input-fields'>
+                <input type="email" placeholder='Enter your email' />
+                <br />
+                <input type="password" placeholder='Enter your password' />
+                <br />
+                <input className='login-btn' type="submit" value="Login" />
+            </form>
         </div>
     );
 };
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Register.css';
 
 const Register = () => {
     return (
         <div>
-            <h1>Register</h1>
+            <h1>Please Register!!</h1>
+            <form className='register-input-fields'>
+                <input type="text" placeholder='Your name' />
+                <br />
+                <input type="email" placeholder='Enter your email' />
+                <br />
+                <input type="password" placeholder='Enter your password' />
+                <br />
+                <input className='register-btn' type="submit" value="Register" />
+            </form>
         </div>
     );
 };
